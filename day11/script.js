1276c98c018c391db403b2271957bca8076552b8
Sai Sawant <saisawant2003@gmail.com>
2023-02-18 19:40:39 +0530

    minor fixes

--- day11/script.js
+++ day11/script.js
@@ -2,7 +2,7 @@ const insertkey=document.querySelector('.insert')
 
 window.addEventListener('keydown',(e)=>{
     insertkey.innerHTML=` <div id="key" class="key">
-    ${e.key} 
+    ${e.key===' '? 'space':e.key} 
     <small>event.key</small>
   </div>
   <div id="keyCode" class="key">
