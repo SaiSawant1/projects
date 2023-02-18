d3b81bc2b2adf40fa6d23b957e7e1580b7915bec
Sai Sawant <saisawant2003@gmail.com>
2023-02-18 14:28:03 +0530

    project day 9 complete

---
+++ day9/script.js
@@ -0,0 +1,24 @@
+const sounds=['applause','boo','gasp','tada','victory','wrong']
+const container=document.querySelector('.container')
+sounds.forEach(sound=>{
+    const btn=document.createElement('button');
+    btn.classList.add('btn');
+    btn.innerText=sound;
+
+    btn.addEventListener('click',()=>{
+        stopSongs()
+        document.getElementById(`${sound}`).play()
+    })
+
+    container.appendChild(btn)
+
+})
+
+function stopSongs() {
+    sounds.forEach(sound=>{
+        const song=document.getElementById(`${sound}`)
+        song.pause()
+        song.currentTime=0;
+        
+    })
+}
