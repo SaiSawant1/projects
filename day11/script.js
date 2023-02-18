110cd5772512377d747bb20f23b9b083af771c4c
Sai Sawant <saisawant2003@gmail.com>
2023-02-18 19:34:55 +0530

    day 11 complete

---
+++ day11/script.js
@@ -0,0 +1,18 @@
+const insertkey=document.querySelector('.insert')
+
+window.addEventListener('keydown',(e)=>{
+    insertkey.innerHTML=` <div id="key" class="key">
+    ${e.key} 
+    <small>event.key</small>
+  </div>
+  <div id="keyCode" class="key">
+    ${e.keyCode}
+    <small>event.keyCode</small>
+  </div>
+  <div id="code" class="key">
+    ${e.code}
+    <small>event.code</small>
+  </div>`   
+
+})
+
