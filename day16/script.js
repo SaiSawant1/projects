081944c96edd43d4d419ab94e6be1f14694c8cbd
Sai Sawant <saisawant2003@gmail.com>
2023-02-22 18:15:31 +0530

    day 16 project complete

--- day16/script.js
+++ day16/script.js
@@ -1,21 +1,42 @@
-const counters= document.querySelectorAll('.counter')
+const smallCups=document.querySelectorAll('.cup-small');
+const liters=document.querySelector('#liters');
+const percentage=document.querySelector('#percentage');
+const remained=document.querySelector('#remained');
 
-counters.forEach(counter => {
 
-    counter.innerText='0'
-    function updateCounter() {
-        const target = +counter.getAttribute('data-target');
-        const c=+counter.innerText
-
-        const increment=target/1000
-
-        if(c<target){
-            counter.innerText=`${Math.ceil(c+increment)}`
-            setTimeout(updateCounter,2)
+smallCups.forEach((cup,indx)=>{
+    cup.addEventListener('click',()=>{
+        if(cup.classList.contains('full')){
+            smallCups.forEach(cup=>{
+                cup.classList.remove('full');
+            })
         }
-
-    }
-
-    updateCounter()
+        for (let index = 0; index <= indx; index++) {
+          smallCups[index].classList.add('full');
+           updateValue();
+        }
+    })
 });
 
+const updateValue=()=>{
+    const fullCups=document.querySelectorAll('.full').length;
+    const totalCups=smallCups.length;
+
+    if(fullCups==0){
+        percentage.style.visibility='hidden';
+        percentage.style.height="0";
+    }
+    else{
+        percentage.style.visibility="visible"
+        percentage.style.height=`${(fullCups/totalCups)*330}px`
+        percentage.innerText=`${fullCups/totalCups*100}%`
+        liters.innerText=`${(2000-(250*fullCups))/1000}L`
+    }
+    if(fullCups===totalCups){
+        remained.style.visibility="hidden"
+        remained.style.height='0'
+    }
+    else{
+        remained.style.visibility="visible"
+    }
+}
