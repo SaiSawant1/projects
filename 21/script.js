d3f19566433e91d56fd1f54e2c3f1dd7caee0450
Sai Sawant <saisawant2003@gmail.com>
2023-03-01 15:00:40 +0530

    day 21 project complete

--- 21/script.js
+++ 21/script.js
@@ -1,2 +1,38 @@
+const fill=document.querySelector('.fill');
 
+const empties=document.querySelectorAll('.empty');
 
+empties.forEach(empty=>{
+    empty.addEventListener('dragenter',dragEnter)
+    empty.addEventListener('dragleave',dragLeave)
+    empty.addEventListener('drop',dragDrop)
+    empty.addEventListener('dragover',dragOver)
+})
+
+fill.addEventListener('dragstart',dragStart)
+fill.addEventListener('dragend',dragEnd)
+
+function dragStart(){
+    this.className+=" hold"
+    setTimeout(()=>this.className="invisible",0)
+}
+function dragEnd(){
+    this.className="fill"
+    
+}
+function dragEnter(){
+    e.preventDefault()
+}
+function dragLeave(){
+    this.className="empty"
+}
+function dragDrop(){
+    this.className='empty';
+    this.append(fill)
+    
+}
+function dragOver(e){
+    e.preventDefault();
+    this.className+=" hovered"
+
+}
