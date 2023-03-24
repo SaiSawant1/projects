493f98d37c414c179dc2ec718775e10b1dc10900
Sai Sawant <saisawant2003@gmail.com>
2023-03-24 22:32:06 +0530

    password generation completed

--- day31/script.js
+++ day31/script.js
@@ -2,10 +2,11 @@
 const result=document.getElementById('result');
 const lenghtEl=document.getElementById('length')
 const upperEl=document.getElementById('uppercase')
-const lowertEl=document.getElementById('lowercase')
+const lowerEl=document.getElementById('lowercase')
 const numberEl=document.getElementById('numbers')
 const symbolsEl=document.getElementById('symbols')
 const clipBoard=document.getElementById('clipboard')
+const generateBtn=document.querySelector('.btn-large')
 
 const randomFunc=  {   
 
@@ -15,7 +16,39 @@ const randomFunc=  {
     symbols: getrandomSymbols
 
 }
+generateBtn.addEventListener('click',()=>{
+    const length= +lenghtEl.value
+    const hasLower=lowerEl.checked
+    const hasUpper=upperEl.checked
+    const hasNumber=numberEl.checked
+    const hasSymbol=symbolsEl.checked 
 
+    result.innerText=generatePasswrd(hasLower,hasUpper,hasNumber,hasSymbol,length) 
+      
+
+})
+
+function generatePasswrd(lower,upper,number,symbols,length){
+    let generatePassword=""
+
+    const typesCount=lower+upper+number+symbols
+
+    const types=[{lower},{upper},{number},{symbols}].filter(item=>Object.values(item)[0])
+
+    if(typesCount===0){
+        return ""
+    }
+
+    for(let i=0;i<length;i+=typesCount){
+
+        types.forEach(type=>{
+            const funcName=Object.keys(type)[0]
+            generatePassword += randomFunc[funcName]()
+         })
+    }
+    const finalPassword=generatePassword.slice(0,length)
+    return finalPassword
+}
 
 
 
