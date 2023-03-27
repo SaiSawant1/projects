97aea33ebe43aa5588e8b64b58df83e512d07731
Sai Sawant <saisawant2003@gmail.com>
2023-03-27 18:51:33 +0530

    day32 poject complete

---
+++ day32/script.js
@@ -0,0 +1,22 @@
+const toggles = document.querySelectorAll(".toggle")
+const good=document.getElementById('good')
+const cheap=document.getElementById('cheap')
+const fast=document.getElementById('fast')
+
+toggles.forEach((toggle)=>{
+    toggle.addEventListener("click",(e)=>{
+        
+        if(good.checked && fast.checked && cheap.checked ){
+            if(good===e.target){
+                fast.checked=false
+            }
+            if(cheap===e.target){
+                good.checked=false
+            }
+            if(fast===e.target){
+                cheap.checked=false
+            }
+            
+        }
+    })
+})
