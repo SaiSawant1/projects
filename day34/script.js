b146f6a8ca75bfa2306c1a71482fcb9e7d8e9a83
Sai Sawant <saisawant2003@gmail.com>
2023-04-08 17:39:07 +0530

    day 34 project complete

--- day34/script.js
+++ day34/script.js
@@ -1,2 +1,38 @@
+const nums=document.querySelectorAll(".nums span");
+const counter=document.querySelector(".counter");
+const finalMessage=document.querySelector(".final");
+const replay=document.querySelector("#replay")
+runAnimation();
 
 
+function resetDom(){
+    counter.classList.remove("hide")
+    finalMessage.classList.remove("show")
+
+    nums.forEach(num => {
+        num.classList.value=""
+    });
+    nums[0].classList.add("in")
+    runAnimation()
+}
+replay.addEventListener("click",()=>resetDom())
+
+function runAnimation(){
+    nums.forEach((num,idx)=>{
+        const nexToLast=nums.length-1
+
+        num.addEventListener("animationend",(e)=>{
+            if(e.animationName=== "goIn" && idx!==nexToLast){
+                num.classList.remove("in")        
+                num.classList.add("out")    
+            }
+            else if(e.animationName==="goOut" && num.nextElementSibling){
+                num.nextElementSibling.classList.add("in")
+            }
+            else{
+                counter.classList.add('hide');
+                finalMessage.classList.add("show")
+            }
+        })
+    })
+}
